302a322b6cc3862c4721331b9dd0c3554deac832
James Keogh <13314592+jameskeo126@users.noreply.github.com>
2026-04-30 20:49:09 +0100

    Add players and course data

---
+++ data/course.ts
@@ -0,0 +1,8 @@
+export const HOLE_PARS: readonly number[] = [
+  4, 5, 4, 3, 4, 3, 4, 5, 4,   // front 9
+  4, 4, 3, 5, 4, 5, 3, 4, 4,   // back 9
+] as const
+
+export const FRONT_9_PAR = 36
+export const BACK_9_PAR = 36
+export const TOTAL_PAR = 72
---
+++ data/players.ts
@@ -0,0 +1,8 @@
+import type { Player } from "./types"
+
+export const PLAYERS: Player[] = [
+  { id: "sam",   displayName: "Sam Clifford",   username: "Sam854",      flag: "🇬🇧" },
+  { id: "josh",  displayName: "Josh Dally",     username: "joshdally",   flag: "🏴󠁧󠁢󠁥󠁮󠁧󠁿" },
+  { id: "jamie", displayName: "Jamie Maclaren", username: "umie51",      flag: "🇦🇺" },
+  { id: "keo",   displayName: "Hames Keo",      username: "JamesKeo126", flag: "🇬🇱" },
+]
